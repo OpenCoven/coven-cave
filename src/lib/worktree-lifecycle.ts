be529dc966bcadaeff1746074d1f027fc47ad73e
Val Alexander <68980965+BunsDev@users.noreply.github.com>
2026-08-04 23:55:43 -0500

    test(worktrees): pin legacy task normalization

--- src/lib/worktree-lifecycle.ts
+++ src/lib/worktree-lifecycle.ts
@@ -192,7 +192,7 @@ export type WorktreeLifecycleItem = NormalizedWorktreeLifecycleObservation & {
 
 type AssertTrue<T extends true> = T;
 export type WorktreeLifecycleTypeContract = [
-  AssertTrue<WorktreeLifecycleObservationInput extends { taskRefs?: WorktreeTaskRef[] } ? true : false>,
+  AssertTrue<{} extends Pick<WorktreeLifecycleObservationInput, "taskRefs"> ? true : false>,
   AssertTrue<WorktreeLifecycleItem["taskRefs"] extends WorktreeTaskRef[] ? true : false>,
 ];
 
